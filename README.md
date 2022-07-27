e818f7d63c9b73c912e678e74d093e259f4fc8ce
Unbiased Coder <info@unbiased-coder.com>
2022-07-27 22:31:01 +0300

    adding guide url

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # python-aws-boto3-subnet
 Python AWS Boto3 Subnet Guide
+
+You can find the full guide here:
+
+https://unbiased-coder.com/boto3-subnet-guide/
--- boto3-subnet-delete.py
+++ boto3-subnet-delete.py
@@ -1,5 +1,5 @@
 import pprint
 from boto3_helper import ec2_delete_subnet
 
-result = ec2_delete_subnet('subnet-0128e4cd0a6ebca54')
+result = ec2_delete_subnet('subnet-0604756b09f4a2dcd')
 pprint.pprint(result)
